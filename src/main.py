b27fe146c047964bca53c9cf7bd4d7ce0a2e6d14
Cezary924 <cezary924@gmail.com>
2024-03-30 12:33:27 +0100

    Read configuration from config.ini file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Hello World!")
+import os
+
+# get path of directory containing bot script
+dir = os.path.dirname(os.path.realpath(__file__)) + "/"
+
+# change current working directory to 'dir'
+os.chdir(dir)
+
+with open("../config/config.ini") as f:
+    config = dict(x.rstrip().split(": ") for x in f)
+
+print(config)
