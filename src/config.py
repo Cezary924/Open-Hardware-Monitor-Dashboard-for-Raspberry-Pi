31aec9174129c7b5ce92b43b6647358a6eee15f2
Cezary924 <cezary924@gmail.com>
2024-04-01 09:32:13 +0200

    Create config.py file

---
+++ src/config.py
@@ -0,0 +1,26 @@
+import json, os
+
+# check if configuration file exists
+def check_file(path: str) -> bool:
+    return os.path.isfile(path)
+
+# create configuration file
+def create_config(path: str) -> None:
+    with open(path, 'w') as f:
+        json.dump({"ip": "XXX.XXX.XXX.XXX", "port": "XXXX", "updateInterval": "XXXXXX"}, f, indent = 4)
+    log.print_log("A config file has been created successfully.", "Please, edit the configuration and run the script again.")
+
+# save configuartion to file
+def save_config(path: str, config_dict: dict) -> None:
+    with open(path, 'w') as f:
+        json.dump(config_dict, f, indent = 4)
+
+# load configuartion from file
+def load_config(path: str) -> dict:
+    with open(path, 'r') as f:
+        loaded_config = json.load(f)
+    return loaded_config
+
+# check correctness of loaded configuration
+def check_correctness(configuration: dict, path: str) -> None:
+    pass
