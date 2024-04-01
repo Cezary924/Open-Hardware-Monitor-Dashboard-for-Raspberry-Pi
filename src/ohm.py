cfb3d5e28601b5e450f323a4cca80013c6720e0e
Cezary924 <cezary924@gmail.com>
2024-04-01 09:27:33 +0200

    Add get_pc_info function

--- src/ohm.py
+++ src/ohm.py
@@ -1,1 +1,12 @@
 import requests
+
+# get PC info from OpenHardwareMonitor Server website
+def get_pc_info(config):
+    res = requests.get("http://" + config['ip'] + ":" + config['port'] + "/data.json")
+    if res.status_code != 200:
+        raise Exception("Connection failed")
+    try:
+        res = res.json()['Children'][0]['Children']
+    except Exception as e:
+        raise Exception(str(e))
+    return res
