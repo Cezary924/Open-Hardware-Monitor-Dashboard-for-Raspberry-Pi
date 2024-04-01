c534fe3a9cc1b174561e7950e9eb7d1ca617a6b8
Cezary924 <cezary924@gmail.com>
2024-04-01 09:29:58 +0200

    Add functions to determine PC values

--- src/fun.py
+++ src/fun.py
@@ -17,3 +17,67 @@ def find_component_loc(text, pc, rev = False):
                 return -1
         return i
 
+def calculate_component_value(pc, text):
+    i = find_component_loc(text, pc)
+    if i == -1:
+        raise Exception("No supported GPU!")
+    return float(pc[i]['Value'][:-2])
+
+def calculate_average_component_value(pc):
+    avg = 0
+    for element in pc:
+        avg = avg + float(element['Value'][:-2])
+    return float(avg/len(pc))
+
+def find_cpu_loc(pc):
+    i = find_component_loc("Intel", pc)
+    if i == -1:
+        i = find_component_loc("AMD", pc)
+    if i == -1:
+        raise Exception("No supported CPU!")
+    return i
+def find_cpu_load(i, pc):
+    j = find_component_loc("Load", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported CPU!")
+    return calculate_average_component_value(pc[i]['Children'][j]['Children'])
+def find_cpu_temp(i, pc):
+    j = find_component_loc("Temperatures", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported CPU!")
+    return calculate_average_component_value(pc[i]['Children'][j]['Children'])
+
+def find_ram_loc(pc):
+    i = find_component_loc("Memory", pc)
+    if i == -1:
+        raise Exception("No supported RAM!")
+    return i
+def find_ram_load(i, pc):
+    j = find_component_loc("Load", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported RAM!")
+    return calculate_average_component_value(pc[i]['Children'][j]['Children'])
+
+def find_gpu_loc(pc):
+    i = find_component_loc("NVIDIA", pc, True)
+    if i == -1:
+        i = find_component_loc("AMD", pc, True)
+    if i == -1:
+        raise Exception("No supported GPU!")
+    return i
+def find_gpu_load(i, pc):
+    j = find_component_loc("Load", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported GPU!")
+    return calculate_component_value(pc[i]['Children'][j]['Children'], "Core")
+def find_gpu_temp(i, pc):
+    j = find_component_loc("Temperatures", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported GPU!")
+    return calculate_average_component_value(pc[i]['Children'][j]['Children'])
+def find_gpu_memload(i, pc):
+    j = find_component_loc("Load", pc[i]['Children'])
+    if j == -1:
+        raise Exception("No supported GPU!")
+    return calculate_component_value(pc[i]['Children'][j]['Children'], "Memory")
+
