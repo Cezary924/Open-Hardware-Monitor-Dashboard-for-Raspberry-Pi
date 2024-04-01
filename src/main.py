5ce2b38c999651fb893599cefe1b06619f9b692f
Cezary924 <cezary924@gmail.com>
2024-04-01 07:25:32 +0200

    Get json data about PC

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import os
+import os, requests
 
 # get path of directory containing bot script
 dir = os.path.dirname(os.path.realpath(__file__)) + "/"
@@ -6,7 +6,15 @@ dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 # change current working directory to 'dir'
 os.chdir(dir)
 
+# read config from configuration file
+#TODO errors
 with open("../config/config.ini") as f:
     config = dict(x.rstrip().split(": ") for x in f)
 
-print(config)
+#TODO errors
+res = requests.get("http://" + config['ip'] + ":" + config['port'] + "/data.json")
+
+if res.status_code != 200:
+    raise Exception("Connection failed")
+
+res = res.json()
