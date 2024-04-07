3994d19624b945decf981542b121d7288cfd259d
Cezary924 <cezary924@gmail.com>
2024-04-07 13:48:08 +0200

    Add exception handler

--- src/ohm.py
+++ src/ohm.py
@@ -4,14 +4,18 @@ import log
 
 # get PC info from OpenHardwareMonitor Server website
 def get_pc_info(config):
-    res = requests.get("http://" + config['ip'] + ":" + config['port'] + "/data.json")
-    if res.status_code != 200:
-        raise Exception("Connection failed")
     try:
-        res = res.json()['Children'][0]['Children']
+        res = requests.get("http://" + config['ip'] + ":" + config['port'] + "/data.json")
     except Exception as e:
-        raise Exception(str(e))
-    return res
+        raise Exception("Connection timed out!")
+    else:
+        if res.status_code != 200:
+            raise Exception("Connection failed!")
+        try:
+            res = res.json()['Children'][0]['Children']
+        except Exception as e:
+            raise Exception(str(e))
+        return res
 
 # print info about error during requesting to Homebridge API
 def print_err(e: Exception) -> None:
